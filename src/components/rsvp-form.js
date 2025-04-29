4d013fcbba77c1ae188e815ca19abd27645c46e3
Terese Mihalcin <teresemihalcin18@gmail.com>
2025-04-28 22:55:27 -0400

    bootstrap form

--- src/components/rsvp-form.js
+++ src/components/rsvp-form.js
@@ -1,33 +1,42 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
-function RsvpForm() {
+export default function Rsvpform() {
+  function rsvpform(formData) {
+    const first = formData.get("first");
+    const last = formData.get("last");
+    alert(`you submitted: '${first} ${last}'`);
+    // here you'll submit to a form, that you'll display on a page, with export options
+  }
   return (
-    <Form class="d-flex">
+    <Form class="d-flex" action={rsvpform}>
       <Form.Group className="mb-3" controlId="formBasicFirstName">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter first name" />
-        <Form.Text className="text-muted">
-          Just be honest
-        </Form.Text>
+      <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" name="first" placeholder="" />
       </Form.Group>
-
       <Form.Group className="mb-3" controlId="formBasicLastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter last name" />
-        <Form.Text className="text-muted">
-          Dont lie to me
-        </Form.Text>
+      <Form.Label>Last Name</Form.Label>
+         <Form.Control type="text" name="last" placeholder="" />
       </Form.Group>
-      <Form.Group className="mb-3 flex-grow-1" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <Form.Group className="mb-3" controlId="formBasicOthers">
+      <Row>
+        <Form.Label column="lg">Party Members/Plus ones?</Form.Label>
+      </Row>
+      <Form.Label>How Many?</Form.Label>
+      <Form.Control type="number" name="points" id="points" placeholder="0"/>
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Form.Group className="mb-3" controlId="formBasicNames">
+      <Form.Label>Member Names</Form.Label>
+      <Form.Control type="textarea" as="textarea" name="party-names" rows="3" placeholder="Enter names of other party/family members"/>
+      </Form.Group>
+      
+      <Button className="mb-3" variant="primary" type="submit">
         Submit
       </Button>
     </Form>
   );
 }
 
-export default RsvpForm;
+//export default Rsvpform;
