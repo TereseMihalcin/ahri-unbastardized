87cdd2df4179ff21cdfa266c030aab8345a27693
Terese Mihalcin <teresemihalcin18@gmail.com>
2025-05-04 21:24:45 -0400

    add silly hidden input field

--- src/components/rsvp-form.js
+++ src/components/rsvp-form.js
@@ -31,6 +31,7 @@ export default function Rsvpform() {
 //  }
   return (
     <Form name="rsvp" method="POST" data-netlify="true" >
+      <input type="hidden" name="form-name" value="rsvp" />
       <Form.Group className="mb-3" controlId="formBasicFirstName">
       <Form.Label>First Name</Form.Label>
         <Form.Control type="text" name="first" placeholder="" />
