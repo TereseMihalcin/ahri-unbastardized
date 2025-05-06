cfdf60503e410d430fa6b827d742ede28cee3647
Terese Mihalcin <teresemihalcin18@gmail.com>
2025-05-05 22:59:05 -0400

    footer and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/navigation.css';
 import './styles/homepage.css';
 import './styles/rsvp.css';
+import './styles/footer.css';
 import Home from "./pages";
 import Rsvp from "./pages/rsvp";
 import Navigation from "./components/navigation";
+import Footer from "./components/footer";
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,6 +14,7 @@ import {
 
 function App() {
   return (
+    <>
       <Router>
         <Navigation />
         <Routes>
@@ -19,6 +22,9 @@ function App() {
             <Route exact path="/rsvp" element={<Rsvp />} />
         </Routes>
       </Router>
+
+      <Footer />
+      </>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+
+export default function Footer() {
+  return (
+    <>
+    <div class="footer">
+      <div class="d-flex p-3">
+        <p class="p-1">Attribution:</p>
+        <a class="p-1 attribution" href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">Free SVG Backgrounds and Patterns by SVGBackgrounds.com</a>
+      </div>
+    </div>
+    
+    </>
+  );
+}
+
+//export default Rsvpform;
