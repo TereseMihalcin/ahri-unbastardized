c0c9a4b9d6e736a75da573e63fba05950578eee1
Terese Mihalcin <teresemihalcin18@gmail.com>
2026-04-17 17:59:42 -0400

    Remove errors, test rsvp

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,9 @@ export default function Footer() {
   return (
     <>
     <div class="footer">
-      <div class="d-flex p-3">
+      <div class="d-flex flex-column p-2">
         <p class="p-1">Attribution:</p>
+        <a class="p-1 attribution" href="https://webneko.net">This is where I got the little cat that looks like Ahri</a>
         <a class="p-1 attribution" href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">Free SVG Backgrounds and Patterns by SVGBackgrounds.com</a>
       </div>
     </div>
