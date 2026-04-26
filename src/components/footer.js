0470e47f6563b9f79564f6dd6cde2de33e7111a2
Terese Mihalcin <teresemihalcin18@gmail.com>
2026-04-26 11:52:43 -0400

    wording changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <div class="footer">
       <div class="d-flex flex-column p-2">
         <h4>Credit</h4>
-        <p><a class="p-1 attribution" href="https://webneko.net">This is where I got the little cat that looks like Ahri</a></p>
+        <p><a class="p-1 attribution" href="https://webneko.net">The little corner cat that looks like Ahri</a></p>
         <p><a class="p-1 attribution" href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">Free SVG Backgrounds and Patterns by SVGBackgrounds.com</a></p>
         <p><a class="p-1 attribution" href="https://codepen.io/amin120104/pen/eYpMjoz">Codepen Countdown</a></p>
       </div>
